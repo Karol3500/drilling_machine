99893aabea671d34ca228058e9c40e94ae6ff062
Karol3500 <kposila@gmail.com>
2015-04-21 18:59:46 +0200

    Added new package CoreTest to git

---
+++ core/src/test/java/pl/edu/pwr/aic/dmp/alg/CoreTest.java
@@ -0,0 +1,21 @@
+package pl.edu.pwr.aic.dmp.alg;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import pl.edu.pwr.aic.dmp.alg.City;
+import pl.edu.pwr.aic.dmp.alg.Core;
+
+public class CoreTest {
+
+	@Test
+	public void shouldCalculateIntervalCorrectly(){
+		Core c = new Core(new ArrayList<City>(),false) {};
+		
+		c.calculateInterval(4, 20, 3184, 0.8, 50);
+		//TODO: investigate why the hell this test pass
+		assertEquals(0, c.getInterwal());
+	}
+}
